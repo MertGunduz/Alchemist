2718fd102851fdabe393161d8402ec0d3d0d92e4
Mert Gunduz <merttgg@gmail.com>
2021-08-11 22:53:38 +0300

    Alchemist Start

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using Console = Colorful.Console;
+
+namespace Alchemist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console Options
+            Console.Title = "Alchemist";
+            Console.WindowHeight = 50;
+            Console.WindowWidth = 30;
+
+            // Console UI
+            Console.WriteLine(" ");
+            Console.WriteLine(" ▄▄▄       ██▓     ▄████▄   ██░ ██ ▓█████  ███▄ ▄███▓ ██▓  ██████ ▄▄▄█████▓", Color.FromArgb(143, 255, 194));
+            Console.WriteLine("▒████▄    ▓██▒    ▒██▀ ▀█  ▓██░ ██▒▓█   ▀ ▓██▒▀█▀ ██▒▓██▒▒██    ▒ ▓  ██▒ ▓▒", Color.FromArgb(113, 228, 165));
+            Console.WriteLine("▒██  ▀█▄  ▒██░    ▒▓█    ▄ ▒██▀▀██░▒███   ▓██    ▓██░▒██▒░ ▓██▄   ▒ ▓██░ ▒░", Color.FromArgb(88, 211, 143));
+            Console.WriteLine("░██▄▄▄▄██ ▒██░    ▒▓▓▄ ▄██▒░▓█ ░██ ▒▓█  ▄ ▒██    ▒██ ░██░  ▒   ██▒░ ▓██▓ ░ ", Color.FromArgb(72, 194, 127));
+            Console.WriteLine(" ▓█   ▓██▒░██████▒▒ ▓███▀ ░░▓█▒░██▓░▒████▒▒██▒   ░██▒░██░▒██████▒▒  ▒██▒ ░ ", Color.FromArgb(58, 175, 111));
+            Console.WriteLine(" ▒▒   ▓▒█░░ ▒░▓  ░░ ░▒ ▒  ░ ▒ ░░▒░▒░░ ▒░ ░░ ▒░   ░  ░░▓  ▒ ▒▓▒ ▒ ░  ▒ ░░   ", Color.FromArgb(46, 150, 92));
+            Console.WriteLine("  ▒   ▒▒ ░░ ░ ▒  ░  ░  ▒    ▒ ░▒░ ░ ░ ░  ░░  ░      ░ ▒ ░░ ░▒  ░ ░    ░    ", Color.FromArgb(34, 131, 77));
+            Console.WriteLine("  ░   ▒     ░ ░   ░         ░  ░░ ░   ░   ░      ░    ▒ ░░  ░  ░    ░      ", Color.FromArgb(24, 109, 62));
+            Console.WriteLine("      ░  ░    ░  ░░ ░       ░  ░  ░   ░  ░       ░    ░        ░           ", Color.FromArgb(16, 88, 48));
+            Console.WriteLine("                  ░                                                        ", Color.FromArgb(11, 61, 33));
+            Console.ReadLine();
+        }
+    }
+}
