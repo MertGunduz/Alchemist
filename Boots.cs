811b256bc4885e2048ffd6e2aa894e00d2b01155
Mert Gunduz <merttgg@gmail.com>
2021-08-13 20:15:17 +0300

    Boots Class Init

---
+++ Boots.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alchemist
+{
+    public class Boots
+    {
+        public Boots(string cBootsName, int cBootsArmor)
+        {
+            bootsName = cBootsName;
+            bootsArmor = cBootsArmor;
+        }
+
+        string bootsName;
+        int bootsArmor;
+    }
+}
