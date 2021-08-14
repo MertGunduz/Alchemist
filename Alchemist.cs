044637c5f5dc5b0ddabc55e0b15fd2a53cfcf0e0
Mert Gunduz <merttgg@gmail.com>
2021-08-14 09:58:47 +0300

    Melee Weapon Class Init

--- Alchemist.cs
+++ Alchemist.cs
@@ -35,7 +35,7 @@ namespace Alchemist
             alchemistBook = cAlchemistBook;
             alchemistRace = cAlchemistRace;
         }
-
+            
         public enum AlchemistRace
         {
             Human,
---
+++ MeleeWeapon.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alchemist
+{
+    public class MeleeWeapon
+    {
+        public MeleeWeapon(string cWeaponAttackDamage)
+        {
+            weaponAttackDamage = cWeaponAttackDamage;
+        }
+
+        public string weaponAttackDamage;
+    }
+}
